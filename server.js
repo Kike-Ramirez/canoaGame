f3d2a67952667f39d3d197108f34e3d5d897222b
Kike-Ramirez <kike@vjspain.com>
2016-07-18 19:44:07 +0200

    2 remos added

--- server.js
+++ server.js
@@ -10,14 +10,20 @@ app.get('/', function(req, res){
   res.sendfile('basicRiver.html');
 });
 
-app.get('/remo', function(req, res){
-  res.sendfile('remo.html');
+app.get('/remo0', function(req, res){
+  res.sendfile('remo0.html');
 });
 
+app.get('/remo1', function(req, res){
+  res.sendfile('remo1.html');
+});
 
 io.on('connection', function(socket){  
   socket.on('acc0', function(msg){
     io.emit('acc0', msg);
+  });
+    socket.on('acc1', function(msg){
+    io.emit('acc1', msg);
   });
 });
 
