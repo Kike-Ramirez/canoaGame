cb41b38196f8822fbdde531d002931b4fe8096d9
miguelespada <miguel.valero.espada@gmail.com>
2016-07-20 17:43:17 +0200

    change fonts

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ io.on('connection', function(socket){
 });
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log(`Listening on ${ process.env.PORT }`);
+  console.log('Listening on 3000');
 });
 
